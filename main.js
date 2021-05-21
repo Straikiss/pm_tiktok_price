1ed36f18885aa20879316dc64e5a8373aa956ea0
Straikiss <starko.web@gmail.com>
2021-05-21 11:58:45 +0300

    1.0.0

--- main.js
+++ main.js
@@ -1,98 +1,72 @@
+// https://www.tiktok.com/@jelyxaly?is_copy_url=1&is_from_webapp=v1
+
+function pm_set(pm_id, pm_value){
+	document.getElementById(pm_id).value = pm_value;
+}
+
+function pm_get(pm_id){
+	return parseFloat(document.getElementById(pm_id).value);
+}
+
 function price(){
-	let price = 0;
 	let rating = 0;
 
-	let VIEWS_1 = parseFloat(document.getElementById("view_1").value);
-	let VIEWS_2 = parseFloat(document.getElementById("view_2").value);
-	let VIEWS_3 = parseFloat(document.getElementById("view_3").value);
+	pm_set("price_per_view", 0.003);
+	pm_set("likes_to_count", 10);
+	pm_set("likes_to_price", 15);
+	pm_set("comments_to_count", 3);
+	pm_set("comments_to_price", 25);
 
-	let LIKES_1 = parseFloat(document.getElementById("like_1").value);
-	let LIKES_2 = parseFloat(document.getElementById("like_2").value);
-	let LIKES_3 = parseFloat(document.getElementById("like_3").value);
+	let VIEWS_1 = pm_get("view_1");
+	let VIEWS_2 = pm_get("view_2");
+	let VIEWS_3 = pm_get("view_3");
 
-	let COMMENTS_1 = parseFloat(document.getElementById("comment_1").value);
-	let COMMENTS_2 = parseFloat(document.getElementById("comment_2").value);
-	let COMMENTS_3 = parseFloat(document.getElementById("comment_3").value);
+	let LIKES_1 = pm_get("like_1");
+	let LIKES_2 = pm_get("like_2");
+	let LIKES_3 = pm_get("like_3");
 
-	let PRICE_PER_VIEW = parseFloat(document.getElementById("price_per_view").value);
-	let SUBSCRIBERS = parseFloat(document.getElementById("subscribers").value);
-	let LIKES_TO_COUNT = parseFloat(document.getElementById("likes_to_count").value);
-	let LIKES_TO_PRICE = parseFloat(document.getElementById("likes_to_price").value);
+	let COMMENTS_1 = pm_get("comment_1");
+	let COMMENTS_2 = pm_get("comment_2");
+	let COMMENTS_3 = pm_get("comment_3");
 
-	let COMMENTS_TO_COUNT = parseFloat(document.getElementById("comments_to_count").value);
-	let COMMENTS_TO_PRICE = parseFloat(document.getElementById("comments_to_price").value);
+	let COMMENTS_TO_COUNT = pm_get("comments_to_count");
+	let COMMENTS_TO_PRICE = pm_get("comments_to_price");
+	let LIKES_TO_COUNT = pm_get("likes_to_count");
+	let LIKES_TO_PRICE = pm_get("likes_to_price");
+	let PRICE_PER_VIEW = pm_get("price_per_view");
 
-	let SUBSCRIBERS_TO_ALL = parseFloat(document.getElementById("subscribers_to_all").value);
-
+	let COMMENTS = COMMENTS_1 + COMMENTS_2 + COMMENTS_3;
 	let VIEWS = VIEWS_1 + VIEWS_2 + VIEWS_3;
 	let LIKES = LIKES_1 + LIKES_2 + LIKES_3;
-	let COMMENTS = COMMENTS_1 + COMMENTS_2 + COMMENTS_3;
 
-	document.getElementById("count_views").value = VIEWS;
-	document.getElementById("count_likes").value = LIKES;
-	document.getElementById("count_comments").value = COMMENTS;
+	pm_set("count_comments", COMMENTS);
+	pm_set("count_views", VIEWS);
+	pm_set("count_likes", LIKES);
+	
+	let price = VIEWS * PRICE_PER_VIEW;
 
-	price = VIEWS * PRICE_PER_VIEW;
+	LIKES_TO_COUNT /= 100;
+	LIKES_TO_PRICE /= 100;
+	COMMENTS_TO_COUNT /= 100;
+	COMMENTS_TO_PRICE /= 100;
 
-	if(LIKES >= (VIEWS / 100) * LIKES_TO_COUNT)
-		price += (price / 100) * LIKES_TO_PRICE;
-	else 
-		price -= (price / 100) * LIKES_TO_PRICE;
+	if(LIKES >= VIEWS * LIKES_TO_COUNT){
+		price += price * LIKES_TO_PRICE;
+		rating += 4;
+	}
+	else{rating += 3;}
 
-	if(COMMENTS >= (VIEWS / 100) * COMMENTS_TO_COUNT)
-		price += (price / 100) * COMMENTS_TO_PRICE;
-	else 
-		price -= (price / 100) * COMMENTS_TO_PRICE;
-
-
-	if((SUBSCRIBERS / 100) * SUBSCRIBERS_TO_ALL <= (VIEWS / 100) * SUBSCRIBERS_TO_ALL) 
-		rating += 0.5;
-	else
-		rating -= 0.5;
-
-	if(LIKES >= (VIEWS / 100) * LIKES_TO_COUNT)
+	if(COMMENTS >= LIKES * COMMENTS_TO_COUNT){
+		price += price * COMMENTS_TO_PRICE;
 		rating += 1;
-	else
-		rating -= 1;
+	}
 
-	if(LIKES >= (VIEWS / 100) * LIKES_TO_COUNT && (SUBSCRIBERS / 100) * SUBSCRIBERS_TO_ALL <= (VIEWS / 100) * LIKES_TO_COUNT)
-		rating += 1.5;
-	else 
-		rating -= 1.5;
-
-	if(COMMENTS >= (VIEWS / 100) * COMMENTS_TO_COUNT)
-		rating += 2;
-	else
-		rating -= 2;
-
-	if(COMMENTS >= (VIEWS / 100) * COMMENTS_TO_COUNT && (SUBSCRIBERS / 100) * SUBSCRIBERS_TO_ALL <=  (VIEWS / 100) * COMMENTS_TO_COUNT)
-		rating += 2.5;
-	else
-		rating -= 2.5;
+	if(rating == 5){price *= 2}
+	if(rating >= 4){price *= 1.5;}
+	if(rating >= 3 && rating < 4){price /= 1.5;}
 
 	if(rating < 0)
 		rating = 0
-
-	if(rating > 5)
-		rating = 5
-
-	if(rating == 5) 
-		price *= 1.5
-	
-	if(rating >= 4 && rating < 5)
-		price *= 1.25
-
-	if(rating >= 3 && rating < 4)
-		price *= 1.25
-	
-	if(rating >= 2 && rating < 3)
-		price /= 1.5
-
-	if(rating >= 1 && rating < 2) 
-		price /= 1.75
-	
-	if(rating == 0 || rating < 1)
-		price /= 2
 
 	document.getElementById("blogger_rating").value = rating + " из 5";
 	document.getElementById("price_per_task").value = parseInt(price) + "р";
